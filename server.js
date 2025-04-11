8b9718b8dfed67e86c4ab6ab36b5648fcf7f4b38
sanketLocal <sanketgdorle200105@gmial.com>
2025-04-11 22:30:28 +0530

    commit server

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require('express');
-const dotenv = require('dotenv');
 const cors = require('cors');
+const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
 const taskRoutes = require('./routes/taskRoutes');
 
-dotenv.config();
-connectDB();
+
 
 const app = express();
-
 app.use(cors({
     origin: '*',  
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     // credentials: true,  // Enable credentials
-  }));
+}));
+dotenv.config();
   
-app.use(express.json());
+  app.use(express.json());
+  connectDB();
 // Root route handler
 app.get("/", (req, res) => {
     res.send("Hello, world! This is the root route.");
